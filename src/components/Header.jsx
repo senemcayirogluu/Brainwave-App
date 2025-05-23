9d8d30191c8246c5cb25a64a22529f785ac25d63
senemcayirogluu <senemcayiroglu@gmail.com>
2025-05-24 00:32:09 +0300

    Collaboration and Services components added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,7 +29,7 @@ const Header = () => {
 
   return (
     <div
-      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:vackdrop-blur-sm ${
+      className={`fixed top-0 left-0 w-full z-50 border-b border-n-6 lg:bg-n-8/90 lg:backdrop-blur-sm ${
         openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop:blur-sm"
       }`}
     >
@@ -50,7 +50,7 @@ const Header = () => {
                 href={item.url}
                 onClick={handleClick}
                 className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 ${
-                  item.onlyMobile ? "lg.hidden" : ""
+                  item.onlyMobile ? "lg:hidden" : ""
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
                   item.url === pathname.hash
                     ? "z-2 lg:text-n-1"

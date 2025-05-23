54fbb3247fd98c5f0ae885461b69deb1396f914e
senemcayirogluu <senemcayiroglu@gmail.com>
2025-05-23 23:24:16 +0300

    Section, Hero, Benefits components added

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import ButtonSvg from "../assets/svg/ButtonSvg";
 
-const Button = ({ className, children, px, white, href }) => {
+const Button = ({ className, children, px, white, href, onClick }) => {
   const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
     px || "px-7"
   } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
   const spanClasses = "relative z-10";
 
   const renderButton = () => (
-    <button className={classes}>
+    <button className={classes} onClick={onClick}>
       <span className={spanClasses}>{children}</span>
       {ButtonSvg(white)}
     </button>
